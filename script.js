5c10852b40dcce7230753d9ad2558c8f97c95cbb
shahfawadkhan <you@sfawadkhan92@gmail.com>
2024-09-04 01:01:18 -0700

    creat an array for questions options and correct answers

---
+++ script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "What does CSS stand for?",
+        options: [
+            "Cascading Style Sheets",
+            "Colorful Style Sheets",
+            "Creative Style Sheets",
+            "Computer Style Sheets"
+        ],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        question: "Which property is used to change the background color?",
+        options: [
+            "color",
+            "background-color",
+            "bgcolor",
+            "background"
+        ],
+        answer: "background-color"
+    },
+    {
+        question: "How do you center a block element horizontally?",
+        options: [
+            "text-align: center;",
+            "margin: 0 auto;",
+            "display: flex;",
+            "padding: 0 auto;"
+        ],
+        answer: "margin: 0 auto;"
+    },
+    {
+        question: "Which of the following CSS properties controls the text size?",
+        options: [
+            "font-style",
+            "text-size",
+            "font-size",
+            "text-style"
+        ],
+        answer: "font-size"
+    },
+    {
+        question: "How do you make each word in a text start with a capital letter?",
+        options: [
+            "text-transform: capitalize;",
+            "text-transform: uppercase;",
+            "text-transform: lowercase;",
+            "text-transform: none;"
+        ],
+        answer: "text-transform: capitalize;"
+    }
+];
